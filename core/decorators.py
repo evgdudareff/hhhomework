8b652265ee03a1bb2609545592ec7b3a6653e582
Evgeny Dudarev <evgduda@mail.ru>
2020-01-16 23:08:57 +0300

    Did homework

--- core/decorators.py
+++ core/decorators.py
@@ -1,5 +1,14 @@
-def cache_decorator():
+def cache_decorator(func, *args):
     # Реализовать декоратор который кэширует результаты вызова функции (есть в лекции)
     # Применить для функции calculator (в calculator.py уже есть import функции cache_decorator)
     # Настоятельно прошу написать декоратор руками, а не копировать, т.к. важно запомнить как это работает
-    pass
+
+    cach = {}
+
+    def wrapperFunction(*args):
+        if args not in cach:
+            cach[args] = func(*args)
+            return cach[args]
+        else:
+            return cach[args]
+    return wrapperFunction
--- core/generators.py
+++ core/generators.py
@@ -1,5 +1,29 @@
+import random
+
 # написать генераторную функцию, которая принимает число N возвращает N рандомных чисел от 1 до 100
+
+
 def gen(N):
-    pass
+    i = 0
+    while i < N:
+        yield random.randint(1, 100)
+        i += 1
+
+
+generatorFunc = gen(3)
+while True:
+    try:
+        print(next(generatorFunc))
+    except StopIteration:
+        break
+
+print('\n\n')
 
 # написать генераторное выражение, которое делает то же самое
+N = 4
+generator = (random.randint(1, 100) for i in range(0, N))
+while True:
+    try:
+        print(next(generator))
+    except StopIteration:
+        break
